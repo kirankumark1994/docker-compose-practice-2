39b04e1ddaba60440a04ee5ec938d25bb0b94557
kiran <kiran@gmail.com>
2023-01-18 08:16:49 +0000

    add script

---
+++ health.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+VAR_1=$(docker compose ps --filter status=running | awk 'F " " {print $4}' | sed -n "2p")
+VAR_2=$(docker compose ps --filter status=running | awk 'F " " {print $5}'  | sed -n "3p")
+
+
+
+
+
+if [ $VAR_1 = running ] && [ $VAR_2 = running ]
+then
+        echo "Deployement is successful"
+else
+        echo "Deployement is failure"
+fi
+
